de9e61240065eaa1c1852df08a50e5e8f488a7d2
JH <jhcode2022@gmail.com>
2023-01-27 10:11:48 +0900

    Add ProfessorTest skeleton code

---
+++ UniversityManagement/src/test/java/org/example/management/ProfessorTest.java
@@ -0,0 +1,21 @@
+package org.example.management;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+Student ID format:
+PERSONTYPE_DEPARTMENT_SEQUENCE
+10_301_00001
+ */
+class ProfessorTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+}
